13678253cadf27cc6d2fa8b49260395fc821fb61
Federico Barresi <federico.barresi@evopro-ag.de>
2018-01-02 22:23:05 +0100

    added long description to setup

--- setup.py
+++ setup.py
@@ -8,9 +8,16 @@ def get_version(filename):
     metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
     return metadata['version']
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='pynmcli',
 	version=get_version('pynmcli/__init__.py'),
 	description='Simple python interface to Network Manager CLI',
+	long_description=readme(),
 	license='MIT',
 	author='Federico Barresi',
 	author_email='fede.barresi@gmail.com',
