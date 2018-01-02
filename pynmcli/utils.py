6cbd80d8cdfd257cf39829d739b7b53d4d599fc8
Federico Barresi <federico.barresi@evopro-ag.de>
2018-01-02 13:18:12 +0100

    fixed some more flake8 issues

--- pynmcli/utils.py
+++ pynmcli/utils.py
@@ -16,14 +16,14 @@ def get_data(table):
     for i in range(1, len(lines)):
         di = dict()
         line = lines[i]
-        if line == '' : 
+        if line == '':
             continue
         for h in range(0, len(headers)):
             header = headers[h]
             start_index = header_line.find(header)
-            if h >= len(headers) -1 :
+            if h >= len(headers) - 1:
                 di[header] = line[start_index:].strip()
-            else :
+            else:
                 end_index = header_line.find(headers[h + 1])
                 di[header] = line[start_index:end_index].strip()
         data.append(di)
