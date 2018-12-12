7e053384ac4ab1a9c3ba05474566cb4617857a57
Kristian Binder <kristian.binder@ekupd.com>
2018-12-12 19:12:16 +0100

    changed utils.get_header() to return not only header string, but also column start position. When table header names are substrings of each other, start position could not be identified correctly in utils.get_data() function (example: nmcli radio - returns columns WIFI, WIFI-HW, WWAN, WWAN-HW). This should fix the issue by remembering the position where the header name was found.

--- pynmcli/utils.py
+++ pynmcli/utils.py
@@ -4,7 +4,14 @@ import re
 def get_header(table):
     header_line = table.split('\n')[0]
     regex_word = r'[a-zA-Z0-9_-]+'
-    headers = re.findall(regex_word, header_line)
+    headers = []
+    header_matches = re.finditer(regex_word, header_line)
+    for header in header_matches:
+        headers.append({
+            'title': header.group(),
+            'start': header.start()
+        })
+
     return headers
 
 
@@ -19,12 +26,12 @@ def get_data(table):
         if line == '':
             continue
         for h in range(0, len(headers)):
-            header = headers[h]
-            start_index = header_line.find(header)
+            header = headers[h]['title']
+            start_index = headers[h]['start']
             if h >= len(headers) - 1:
                 di[header] = line[start_index:].strip()
             else:
-                end_index = header_line.find(headers[h + 1])
+                end_index = headers[h + 1]['start']
                 di[header] = line[start_index:end_index].strip()
         data.append(di)
     return data
